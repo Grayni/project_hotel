7f7b2eb33bb6ead0c640ec0cbab4725cf422429b
Grayni <omenbestg@gmail.com>
2019-03-17 13:33:50 +0300

    changing structure of file for application of slot. name: services

--- src/components/sections/services/rooms/S1AboutRooms.vue
+++ src/components/sections/services/rooms/S1AboutRooms.vue
@@ -1,7 +1,8 @@
 <template lang="pug">
-  div.bedroom
-    img.background(src="/static/backgrounds/bedroom.jpg")
-    div.bed-text
+  Services
+    template(#img-background="")
+      img.background(src="/static/backgrounds/bedroom.jpg")
+    template(#text-background="")
       p База отдыха «Уржум» предлагает своим гостям настоящее гостеприимство:
       p -домашняя обстановка и уют
       p -современный дизайн номеров
@@ -11,35 +12,12 @@
 </template>
 
 <script>
+import Services from '@/components/slots/Services'
+
 export default {
-  name: 's1-about-room'
+  name: 's1-about-room',
+  components: { Services }
 }
 </script>
 
-<style lang="stylus" scoped>
-  .bedroom
-    position relative
-    margin-top 10vh
-    width 100vw
-  .background
-    max-width 100vw
-  .bed-text
-    position absolute
-    z-index 1
-    top 0
-    height auto
-    width 46%
-    min-width 500px
-    padding 20px
-    right 5vw
-    top 10vh
-    margin auto
-    border-radius 3px
-    background-color rgba(255, 255, 255, .5)
-    font 18px Academic
-    text-transform uppercase
-    line-height 30px
-    letter-spacing: 6px;
-    text-transform: uppercase;
-    color: #3e3937;
-</style>
+<style></style>
